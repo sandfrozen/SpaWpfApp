e754cd1be5a2beaf19af8a4fe4740789fd45a82f
Rawenvoys <lukwis95@gmail.com>
2018-04-15 18:54:01 +0200

    Query Preprocessor not testet without Tree

--- SpaWpfApp/PqlModels/PqlQueryTree.cs
+++ SpaWpfApp/PqlModels/PqlQueryTree.cs
@@ -13,25 +13,6 @@ namespace SpaWpfApp.PqlModels
 
         public PqlQueryTree(QueryPreprocessor qP)
         {
-            CallType cT = new CallType();
-            ArgType aT = new ArgType();
-            foreach (var line in qP.Query.Lines)
-            {
-                var keys = line.Split(' ');
-                foreach(var key in keys)
-                {
-                    if (aT.List.Contains(key))
-                    {
-
-                    }
-
-                    if (cT.List.Contains(key))
-                    {
-
-                    }
-                }
-
-            }
         }
     }
 }
--- SpaWpfApp/QueryPreprocessor.cs
+++ SpaWpfApp/QueryPreprocessor.cs
@@ -24,22 +24,37 @@ namespace SpaWpfApp
 
         public Query(string query)
         {
-            this.Lines = SeparateLines(query.ToLower());
+            this.Lines = SeparateLines(query);
         }
 
+        /// <summary>
+        /// Parsing Query to Lines
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
         public List<String> SeparateLines(string query)
         {
-            List<String> result = new List<String>();
+            List<String> tmpList = new List<String>();
             string[] lines = Regex.Split(query, "\r\n");
             foreach (var line in lines)
             {
                 string[] partsOfLine = Regex.Split(line, ";");
                 foreach(var part in partsOfLine)
                 {
-                    result.Add(part.Trim());
+                    var keys = part.Split(' ');
+                    foreach(var key in keys)
+                        tmpList.Add(key.ToLower().Replace('(', ' ').Replace(')', ' ').Trim());
                 }
             }
-            result.RemoveAll(x=>x == string.Empty);
+            tmpList.RemoveAll(x=>x == string.Empty);
+            List<String> result = new List<String>();
+            foreach(var tmp in tmpList)
+            {
+                var keys = tmp.Split(',');
+                foreach (var key in keys)
+                    result.Add(key);
+            }
+            result.RemoveAll(x => x == string.Empty);
             return result;
         }
     }
