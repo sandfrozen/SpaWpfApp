a2c1686f8396d578b52db11e72f8af65062d3980
Adam Sujeta <adamsujeta@gmail.com>
2018-04-16 13:29:46 +0200

    Merge branch 'master' into pql

---
+++ SpaWpfApp/Enums/TLinkTypeEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaWpfApp.Enums
+{
+    public enum TLinkTypeEnum
+    {
+        Up,
+        FirstChild,
+        RightSibling
+    }
+}
---
+++ SpaWpfApp/Exceptions/WrongCodeException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace SpaWpfApp.Exceptions
+{
+    class WrongCodeException : Exception
+    {
+        public WrongCodeException()
+        {
+        }
+
+        public WrongCodeException(string message) : base(message)
+        {
+        }
+
+        public WrongCodeException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected WrongCodeException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
