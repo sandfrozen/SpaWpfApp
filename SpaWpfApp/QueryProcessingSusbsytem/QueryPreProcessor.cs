44c99f142a2cc50a6166dd1790078cd56133e779
Tomek (W101) <tbuslowski@gmail.com>
2018-04-30 20:19:16 +0200

    Parsing BOOLEAN and other fixes

--- SpaWpfApp/QueryProcessingSusbsytem/QueryPreProcessor.cs
+++ SpaWpfApp/QueryProcessingSusbsytem/QueryPreProcessor.cs
@@ -16,6 +16,8 @@ namespace SpaWpfApp.QueryProcessingSusbsytem
         private int currentIndex;
 
         private Dictionary<string, string> declarationsList;
+        private Dictionary<string, string> returnList;
+        private Dictionary<string, string[]> entityAttributeValue;
         private Dictionary<string, Action> declarationActions;
 
         private static QueryPreProcessor instance;
@@ -31,6 +33,17 @@ namespace SpaWpfApp.QueryProcessingSusbsytem
         public QueryPreProcessor()
         {
             declarationsList = new Dictionary<string, string>();
+            returnList = new Dictionary<string, string>();
+            entityAttributeValue = new Dictionary<string, string[]>
+            {
+                {"procedure", new string[] { "procName", "string" } },
+                {"stmt", new string[] { "stmt#", "int" } },
+                {"assign", new string[] { "stmt#", "int" } },
+                {"call", new string[] { "procName", "string" } },
+                {"variable", new string[] { "varName", "string" } },
+                {"constatnt", new string[] { "value", "int" } },
+            };
+
             declarationActions = new Dictionary<string, Action>{
               {"procedure", ParseDeclaration},
               {"stmtLst", ParseDeclaration},
@@ -48,12 +61,14 @@ namespace SpaWpfApp.QueryProcessingSusbsytem
         public string Parse(string query)
         {
             declarationsList = new Dictionary<string, string>();
+            returnList = new Dictionary<string, string>();
             parsedQuery = "";
             if (!query.Contains("Select"))
             {
                 throw new WrongQueryFromatException("Select is missing.");
             }
             wordsInQuery = GetWordsInCode(query);
+            currentIndex = 0;
             if (wordsInQuery[0] != "Select")
             {
                 for (currentIndex = 0; currentIndex < wordsInQuery.Length; currentIndex++)
@@ -69,7 +84,7 @@ namespace SpaWpfApp.QueryProcessingSusbsytem
                     }
                     else
                     {
-                        throw new WrongQueryFromatException("Invalid word: " + wordsInQuery[currentIndex]);
+                        throw new WrongQueryFromatException("Invalid word or character in declaration: " + wordsInQuery[currentIndex]);
                     }
                 }
             }
@@ -113,6 +128,11 @@ namespace SpaWpfApp.QueryProcessingSusbsytem
 
             Trace.WriteLine("Declarations:");
             foreach (var v in declarationsList)
+            {
+                Trace.WriteLine(v.Value + " " + v.Key);
+            }
+            Trace.WriteLine("Return List:");
+            foreach (var v in returnList)
             {
                 Trace.WriteLine(v.Value + " " + v.Key);
             }
@@ -171,39 +191,37 @@ namespace SpaWpfApp.QueryProcessingSusbsytem
                     switch (attrName)
                     {
                         case "stmt#":
-                            if (designEntity != "assign")
-                            {
+                            if (designEntity != "assign" && designEntity != "stmt")
                                 throw new QueryException("Synonym: " + synonym + " can't be used with: " + attrName);
-                            }
                             break;
                         case "procName":
-                            if (designEntity != "procedure")
-                            {
+                            if (designEntity != "procedure" && designEntity != "call")
                                 throw new QueryException("Synonym: " + synonym + " can't be used with: " + attrName);
-                            }
                             break;
                         case "varName":
-                        case "value":
                             if (designEntity != "variable")
-                            {
                                 throw new QueryException("Synonym: " + synonym + " can't be used with: " + attrName);
-                            }
+                            break;
+                        case "value":
+                            if (designEntity != "constant")
+                                throw new QueryException("Synonym: " + synonym + " can't be used with: " + attrName);
                             break;
                         default:
                             throw new QueryException("Attrybute name: " + attrName + " is unknown");
-                            break;
                     }
+                    returnList.Add(tuple, designEntity);
                 }
                 else
                 {
                     CkeckIsDeclared(tuple);
+                    returnList.Add(tuple, declarationsList[tuple]);
                 }
             }
         }
 
         private void CheckMultipleTuple(string tuple)
         {
-            for (int elemIndex = 1; elemIndex < tuple.Length; elemIndex++)
+            for (int elemIndex = 1; elemIndex < tuple.Length-1; elemIndex++)
             {
                 string elem = "";
                 do
@@ -294,8 +312,13 @@ namespace SpaWpfApp.QueryProcessingSusbsytem
             {
                 tuple += wordsInQuery[currentIndex++];
             } while (currentIndex < wordsInQuery.Length && wordsInQuery[currentIndex] != "such" && wordsInQuery[currentIndex] != "with" && wordsInQuery[currentIndex] != "pattern");
-
-            CheckSingleTuple(tuple);
+            if(tuple != "BOOLEAN")
+            {
+                CheckSingleTuple(tuple);
+            } else
+            {
+                returnList.Add("BOOLEAN", "BOOLEAN");
+            }
             parsedQuery += " " + tuple;
         }
     }
