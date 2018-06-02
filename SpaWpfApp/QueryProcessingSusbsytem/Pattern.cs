e0ab6bba16b6423b80f34537ceeb61bcc053f240
Tomek (W101) <tbuslowski@gmail.com>
2018-06-02 21:39:22 +0200

    Pattern is working

--- SpaWpfApp/QueryProcessingSusbsytem/Pattern.cs
+++ SpaWpfApp/QueryProcessingSusbsytem/Pattern.cs
@@ -7,6 +7,35 @@ namespace SpaWpfApp.QueryProcessingSusbsytem
 {
     class Pattern : Condition
     {
+        // It looks like:
+        // pattern a("x", "2")
+        // a is synonym
+        // "x" is arg1
+        // "2" is arg2
+        public string synonym { get; set; }
+        public string synonymType { get; set; }
+        public string arg1 { get; set; }
+        public string arg1type { get; set; }
+        public string arg2 { get; set; }
+        public string arg2type { get; set; }
+
+        //arg3 for 'if' is always '_'
+
+        public Pattern(string synonym, string synonymType, string arg1, string arg1type, string arg2, string arg2type)
+        {
+            this.synonym = synonym;
+            this.synonymType = synonymType;
+            this.arg1 = arg1;
+            this.arg1type = arg1type;
+            this.arg2 = arg2;
+            this.arg2type = arg2type;
+        }
+
+
+        override public string ToString()
+        {
+            return this.synonym + "(" + this.arg1 + ", " + this.arg2 + ( synonymType == "if" ? ", _" : "" ) + ")";
+        }
 
     }
 }
