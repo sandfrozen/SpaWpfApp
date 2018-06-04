80716db0ef119ffe65492111c2b6d66f7c627edd
Tomek (W101) <tbuslowski@gmail.com>
2018-06-04 13:56:09 +0200

    ConsoleSpa App Works

--- ConsoleSpa/Program.cs
+++ ConsoleSpa/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using SpaWpfApp.Ast;
+using SpaWpfApp.Cfg;
+using SpaWpfApp.ParserNew;
+using SpaWpfApp.PkbNew;
+using SpaWpfApp.QueryProcessingSusbsytem;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -12,7 +17,7 @@ namespace ConsoleSpa
         static void Main(string[] args)
         {
             string SourceCode = "";
-            SpaWpfApp.PkbNew.PkbAPI pkb;
+            PkbAPI pkb;
             if (args.Length == 0)
             {
                 Console.WriteLine("No parameter (Source.txt)");
@@ -29,16 +34,18 @@ namespace ConsoleSpa
                 SourceCode = File.ReadAllText(path);
                 Console.WriteLine("File Readed ok");
 
-                SourceCode = SpaWpfApp.ParserNew.ParserByTombs.Instance.Parse(SourceCode);
+                SourceCode = ParserByTombs.Instance.Parse(SourceCode);
                 Console.WriteLine("Source Parsed ok");
 
-                pkb = SpaWpfApp.ParserNew.ParserByTombs.Instance.pkb;
+                pkb = ParserByTombs.Instance.pkb;
+                QueryEvaluator.GetInstance().pkb = pkb;
+                QueryProjector.GetInstance().Pkb = pkb;
                 Console.WriteLine("PKB ok");
 
-                SpaWpfApp.Ast.AstManager.GetInstance().GenerateStructures(SourceCode, pkb);
+                AstManager.GetInstance().GenerateStructures(SourceCode, pkb);
                 Console.WriteLine("AST ok");
 
-                SpaWpfApp.Cfg.CfgManager.GetInstance().GenerateStructure(SourceCode, pkb);
+                CfgManager.GetInstance().GenerateStructure(SourceCode, pkb);
                 Console.WriteLine("CFG ok");
 
             }
@@ -57,37 +64,38 @@ namespace ConsoleSpa
 
                 try
                 {
-                    query = SpaWpfApp.QueryProcessingSusbsytem.QueryPreProcessor.GetInstance().Parse(query);
-                    //SpaWpfApp.QueryProcessingSusbsytem.QueryEvaluator.GetInstance().Evaluate(SpaWpfApp.QueryProcessingSusbsytem.QueryPreProcessor.GetInstance().conditionsList);
-                    List<SpaWpfApp.QueryProcessingSusbsytem.Condition> conditionsList = SpaWpfApp.QueryProcessingSusbsytem.QueryPreProcessor.GetInstance().conditionsList;
-                    SpaWpfApp.QueryProcessingSusbsytem.QueryEvaluator.GetInstance().Evaluate(conditionsList);
-                    
-
-                    //Console.WriteLine(SpaWpfApp.QueryProcessingSusbsytem.QueryResult.GetInstance().resultIsBoolean.ToString().ToLower());
+                    query = QueryPreProcessor.GetInstance().Parse(query);
                 }
                 catch (Exception e)
                 {
-                    //Console.WriteLine(e.GetType().Name + ": " + e.Message);
+                    Console.WriteLine(e.GetType().Name + ": " + e.Message);
+                    continue;
                 }
-                finally
+
+                try
                 {
-                    //tutaj QueryProjector wkracza do gry - interpretuje instancję klasy Result
-                    try
-                    {
-                        SpaWpfApp.QueryProcessingSusbsytem.QueryResult queryResult = SpaWpfApp.QueryProcessingSusbsytem.QueryResult.GetInstance();
-                        SpaWpfApp.QueryProcessingSusbsytem.QueryProjector queryProjector = SpaWpfApp.QueryProcessingSusbsytem.QueryProjector.GetInstance();
-
-                        var vfvd = SpaWpfApp.QueryProcessingSusbsytem.QueryPreProcessor.GetInstance();
-                        Console.WriteLine(queryProjector.PrintResult());
-                    }
-                    catch
-                    {
-                        Console.WriteLine("none");
-                    }
-
+                    List<Condition> conditionsList = QueryPreProcessor.GetInstance().conditionsList;
+                    QueryEvaluator.GetInstance().Evaluate(conditionsList);
                 }
-            }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.GetType().Name + ": " + e.Message);
+                    continue;
+                }
+                try
+                {
+                    QueryResult queryResult = QueryResult.GetInstance();
+                    QueryProjector queryProjector = QueryProjector.GetInstance();
 
+                    var vfvd = QueryPreProcessor.GetInstance();
+                    Console.WriteLine(queryProjector.PrintResult());
+                }
+                catch
+                {
+                    Console.WriteLine("none");
+                }
+
+            }
 
             Console.WriteLine("Program exit.");
             Console.ReadKey();
